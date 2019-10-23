02be588ea33aaf08587ffb7e3520c4153a404a2a
fanaasyifa <fanaasyifa@gmail.com>
2019-10-23 17:23:10 +0700

    update pertemuan 9

---
+++ Praktikum-09/Ika Lailatuzzahro/Pertemuan9/src/pertemuan9/interfacelatihan/Program.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pertemuan9.interfacelatihan;
+
+/**
+ *
+ * @author ELA-PC
+ */
+public class Program {
+    public static void main(String[] args) {
+        Rektor pakRektor = new Rektor();
+        
+        //Mahasiswa mahasiswaBiasa = new Mahasiswa("Charlie");
+        Sarjana sarjanaCumlaude = new Sarjana("Dini");
+        PascaSarjana masterCumlaude = new PascaSarjana("Elok");
+        
+        
+        //pakRektor.beriSertifikatCumlaude(sarjanaCumlaude);
+        //pakRektor.beriSertifikatCumlaude(masterCumlaude);
+        
+        pakRektor.beriSertifikatMawapres(sarjanaCumlaude);
+        pakRektor.beriSertifikatMawapres(masterCumlaude);
+    }
+}

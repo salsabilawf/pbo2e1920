5752be91ab4193524e22d3175c35959c2ae5f3a2
achmadfauzannur <56915046+achmadfauzannur@users.noreply.github.com>
2019-10-23 18:23:35 +0700

    update jobsheet9

---
+++ Praktikum-09/Achmad Fauzan Nur/src/job9oj/Sepeda.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package job9oj;
+
+/**
+ *
+ * @author ojan
+ */
+public class Sepeda extends Kendaraan implements IKendaraan{
+    public String merk;
+    public int gear = 0;
+    public int kecepatan = 0;
+    
+    public Sepeda(String jenisKendaraan, String merk){
+        super(jenisKendaraan);
+        this.merk = merk;
+    }
+
+    @Override
+    public void tambahGear() {
+        if (gear>=0) {
+        gear += 1;    
+        }
+        else{
+    }
+}
+
+    @Override
+    public void kurangiGear() {
+        if (gear == 0) {
+            System.out.println("Gear di 0, tidak bisa di kurangi");
+        }
+        else{
+            gear -=1;
+    }
+    }
+    @Override
+    public void tambahKecepatan() {
+        kecepatan += 5;
+    }
+
+    @Override
+    public void rem() {
+        if (kecepatan == 0) {
+            System.out.println("kecepatan masih di angka 0");
+        }else{
+            kecepatan -= 5;
+        }
+    }
+    public void info(){
+        super.info();
+        System.out.println("Merek       : "+this.merk);
+        System.out.println("Gear        : "+this.gear);
+        System.out.println("Kecepatan   : "+this.kecepatan);
+        System.out.println("-------------------------------------");
+    }
+
+}
---
+++ Praktikum-09/Achmad Fauzan Nur/src/job9oj/SepedaMotor.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package job9oj;
+
+/**
+ *
+ * @author ojan
+ */
+public class SepedaMotor extends Kendaraan implements IKendaraan{
+    public String merk;
+    public int gear = 0;
+    public int kecepatan = 0;
+    
+    public SepedaMotor(String jenisKendaraan, String merk){
+        super(jenisKendaraan);
+        this.merk = merk;
+    }
+
+    @Override
+    public void tambahGear() {
+        if (gear==0) {
+        gear += 1;    
+        }
+        else{
+    }
+}
+
+    @Override
+    public void kurangiGear() {
+        if (gear == 0) {
+            System.out.println("Gear di N, tidak bisa di kurangi");
+        }
+        else{
+            gear -=1;
+    }
+    }
+    @Override
+    public void tambahKecepatan() {
+        kecepatan += 15;
+    }
+
+    @Override
+    public void rem() {
+        if (kecepatan == 0) {
+            System.out.println("kecepatan masih di angka 0");
+        }else{
+            kecepatan -= 15;
+        }
+    }
+    public void info(){
+        super.info();
+        System.out.println("Merek       : "+this.merk);
+        System.out.println("Gear        : "+this.gear);
+        System.out.println("Kecepatan   : "+this.kecepatan);
+        System.out.println("-------------------------------------");
+    }
+
+}

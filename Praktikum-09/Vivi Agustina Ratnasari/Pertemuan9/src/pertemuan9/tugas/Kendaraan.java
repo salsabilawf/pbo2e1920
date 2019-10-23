77b8e24cf9b9c5b07d61e02845784aa542818e5a
AgustinaVivi <viviagustinaratnasari34@gmail.com>
2019-10-23 17:19:20 +0700

    update vivi pertemuan 9

---
+++ Praktikum-09/Vivi Agustina Ratnasari/Pertemuan9/src/pertemuan9/tugas/Kendaraan.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pertemuan9.tugas;
+
+/**
+ *
+ * @author Vivi Agustina
+ */
+public class Kendaraan {
+    protected String merk;
+    
+Kendaraan(String merk){
+    this.merk = merk;
+}
+}
